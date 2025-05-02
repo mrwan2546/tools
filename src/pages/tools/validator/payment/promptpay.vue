808f2a892794b7717f674decd0ac98b932040900
mrwan2546 <unturnedmc1@gmail.com>
2025-05-03 00:24:16 +0700

    feat: Add slip verify

--- src/pages/tools/validator/payment/promptpay.vue
+++ src/pages/tools/validator/payment/promptpay.vue
@@ -2,21 +2,29 @@
 import { reactive } from "vue";
 import CardText from "@/components/CardText.vue";
 import CurrencyNum from "@/data/currency_num.json"
+import BankCode from "@/data/banks.json"
 import { type DetectedBarcode, QrcodeCapture } from 'vue-qrcode-reader'
 
 import { parse } from "promptparse"
+import dayjs from "@/utils/dayjs";
+
 
 function onDetect(value: DetectedBarcode[]) {
   const val = value[0].rawValue;
   const parsed = parse(val, true)
   if (parsed) {
-    // Get tag value
+    // Clear data
     payload.valid = true
+    payload.info.desination = ""
+    payload.info.type = ""
     payload.info.amount = ""
     payload.info.message = ""
     payload.info.currency.numId = ""
     payload.info.currency.code = ""
-
+    payload.info.slip.bank.code = ""
+    payload.info.slip.bank.name = ""
+    payload.info.slip.date = ""
+    payload.info.slip.transfer = ""
 
     // Get tag
     const mobile = parsed.getTagValue("29", "01")
@@ -28,35 +36,59 @@ function onDetect(value: DetectedBarcode[]) {
     const currency = parsed.getTagValue("53")
     const message = parsed.getTagValue("81")
 
-    if (mobile) {
-      payload.info.type = "Mobile"
-      payload.info.desination = `+${Number(mobile.substring(0, 4)).toString()}${mobile.substring(4)}`
-    } else if (citizenId) {
-      payload.info.type = "Citizen ID"
-      payload.info.desination = citizenId
-    } else if (eWallet) {
-      payload.info.type = "E-Wallet"
-      payload.info.desination = eWallet
-    } else if (bankAccount) {
-      payload.info.type = "Bank account"
-      payload.info.desination = bankAccount
-    }
+    const tag00 = parsed.getTagValue("00", "00")
+    const tag01 = parsed.getTagValue("00", "01")
+    const tag02 = parsed.getTagValue("00", "02")
+    const tag03 = parsed.getTagValue("00", "03")
+    const tag04 = parsed.getTagValue("00", "04")
 
-    if (amount) {
-      payload.info.amount = amount
-    }
-    if (currency) {
-      payload.info.currency.numId = currency
-      // @ts-expect-error
-      payload.info.currency.code = CurrencyNum[currency]
-    }
-    if (message) {
-      // Parse get text
-      const texts = message.match(/.{1,4}/g) as string[]
-      for (const text of texts) {
-        payload.info.message += String.fromCharCode(parseInt(text, 16))
+    // Check if slip verify
+    if (tag00 && tag01 && tag02) {
+      if (tag02 === "P2P" && tag03 && tag04) {
+        payload.info.kind = "QR Slip (Truemoney)"
+        payload.info.slip.transfer = tag03
+        payload.info.slip.date = dayjs(tag04, "DDMMYYYY").format("DD MMM YYYY")
+      } else {
+        payload.info.kind = "QR Slip (Promptpay)"
+        payload.info.slip.transfer = tag02
+        // @ts-expect-error 
+        payload.info.slip.bank.name = BankCode[tag01] ?? "Unknown"
+        payload.info.slip.bank.code = tag01
+      }
+    } else {
+      payload.info.kind = "QR Promptpay"
+
+      if (mobile) {
+        payload.info.type = "Mobile"
+        payload.info.desination = `+${Number(mobile.substring(0, 4)).toString()}${mobile.substring(4)}`
+      } else if (citizenId) {
+        payload.info.type = "Citizen ID"
+        payload.info.desination = citizenId
+      } else if (eWallet) {
+        payload.info.type = "E-Wallet"
+        payload.info.desination = eWallet
+      } else if (bankAccount) {
+        payload.info.type = "Bank account"
+        payload.info.desination = bankAccount
       }
 
+      if (amount) {
+        payload.info.amount = amount
+      }
+      if (currency) {
+        payload.info.currency.numId = currency
+        // @ts-expect-error
+        payload.info.currency.code = CurrencyNum[currency]
+      }
+      if (message) {
+        payload.info.kind = "QR Truemoney"
+
+        // Parse get text
+        const texts = message.match(/.{1,4}/g) as string[]
+        for (const text of texts) {
+          payload.info.message += String.fromCharCode(parseInt(text, 16))
+        }
+      }
     }
   }
 }
@@ -64,6 +96,7 @@ function onDetect(value: DetectedBarcode[]) {
 const payload = reactive({
   valid: false,
   info: {
+    kind: "", // PromptPay QR, Truemoney QR, Slip
     type: "", // Mobile, E-Wallet, Citizen ID
     desination: "",
     message: "", // (TMN) Message
@@ -72,6 +105,15 @@ const payload = reactive({
       numId: "",
       code: ""
     },
+    slip: {
+      transfer: "",
+      bank: {
+        code: "",
+        name: ""
+      },
+      date: "",
+      type: ""
+    }
   }
 });
 </script>
@@ -93,10 +135,26 @@ const payload = reactive({
           <table class="border border-neutral-700">
             <tbody>
               <tr>
+                <td class="p-3">QR Type</td>
+                <td class="p-3" v-text="payload.info.kind ?? 'Unknown'"></td>
+              </tr>
+              <tr v-if="payload.info.type">
                 <td class="p-3">Type</td>
                 <td class="p-3" v-text="payload.info.type"></td>
               </tr>
-              <tr>
+              <tr v-if="payload.info.slip.transfer">
+                <td class="p-3">Transfer ID</td>
+                <td class="p-3" v-text="payload.info.slip.transfer"></td>
+              </tr>
+              <tr v-if="payload.info.slip.date">
+                <td class="p-3">Transfer date</td>
+                <td class="p-3" v-text="payload.info.slip.date"></td>
+              </tr>
+              <tr v-if="payload.info.slip.bank.code">
+                <td class="p-3">Bank</td>
+                <td class="p-3">{{ payload.info.slip.bank.name }} ({{ payload.info.slip.bank.code }})</td>
+              </tr>
+              <tr v-if="payload.info.desination">
                 <td class="p-3">Desination</td>
                 <td class="p-3" v-text="payload.info.desination"></td>
               </tr>
